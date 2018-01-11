a4c54fc5940676355abc41694f56b40e09ba9603
Carlos Zaldviar Batista <carlos.zaldivar.batista@gmail.com>
2018-01-11 01:50:59 +0100

    Initial commit.

---
+++ src/main/java/com/github/carloszaldivar/distributedauth/DistributedAuthApplication.java
@@ -0,0 +1,12 @@
+package com.github.carloszaldivar.distributedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedAuthApplication.class, args);
+	}
+}
