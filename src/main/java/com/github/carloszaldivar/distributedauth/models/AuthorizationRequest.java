4c423bc60811f5a1180645b5b7a895860224ca7d
Carlos Zaldviar Batista <carlos.zaldivar.batista@gmail.com>
2018-01-23 03:47:21 +0100

    Integrate AuthorizationRequest.

--- src/main/java/com/github/carloszaldivar/distributedauth/models/AuthorizationRequest.java
+++ src/main/java/com/github/carloszaldivar/distributedauth/models/AuthorizationRequest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class AuthorizationRequest {
+    @JsonProperty("Pin")
     final private String pin;
+    @JsonProperty("OneTimePassword")
     final private String oneTimePassword;
 
     @JsonCreator
-    public AuthorizationRequest(@JsonProperty("pin") String pin, @JsonProperty("oneTimePassword") String oneTimePassword) {
+    public AuthorizationRequest(@JsonProperty("Pin") String pin, @JsonProperty("OneTimePassword") String oneTimePassword) {
         this.pin = pin;
         this.oneTimePassword = oneTimePassword;
     }
